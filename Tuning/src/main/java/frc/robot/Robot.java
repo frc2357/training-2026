13b3c69703ac0bdbe4a732df70d9bf56b8581d53
Nathan2351 <nathan.larsen892@raypec.org>
2026-06-16 19:36:52 -0500

    started tuning project

---
+++ Tuning/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.hal.DriverStationJNI;
+import edu.wpi.first.util.WPIUtilJNI;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.internal.DriverStationModeThread;
+
+/**
+ * This class is run automatically. If you change the name of this class or the
+ * package after
+ * creating this project, you must also update the Main.java file in the
+ * project.
+ */
+public class Robot extends RobotBase {
+  public Robot() {
+  }
+
+  public void disabled() {
+  }
+
+  public void autonomous() {
+  }
+
+  public void teleop() {
+  }
+
+  public void test() {
+  }
+
+}
