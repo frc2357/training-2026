60c9c1f2dcd4b8758f662e94fefaad1220923064
Nathan2351 <nathan.larsen892@raypec.org>
2026-07-21 20:07:56 -0500

    Started on first Kraken subsystem and there are different methods of setting speed so please comment to see which is best.

--- LARR-E/src/main/java/frc/robot/subsystems/intakePivot/IntakePivot.java
+++ LARR-E/src/main/java/frc/robot/subsystems/intakePivot/IntakePivot.java
@@ -2,28 +2,27 @@ package frc.robot.subsystems.intakePivot;
 
 import static edu.wpi.first.units.Units.Value;
 import com.revrobotics.spark.SparkMax;
+import com.ctre.phoenix6.hardware.TalonFX;
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.units.measure.Dimensionless;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.CanID;
+import frc.robot.subsystems.intakePivot.IntakePivotConstants;
 
 public class IntakePivot extends SubsystemBase {
 
-    private SparkMax m_motor;
+    private TalonFX m_motor;
+    private final DutyCycleOut m_dutyCycle = new DutyCycleOut(0);
 
     public IntakePivot() {
-        m_motor = new SparkMax(CanID.INTAKE_PIVOT_MOTOR, MotorType.kBrushless);
-
-        m_motor.configure(
-                IntakePivotConstants.MOTOR_CONFIG,
-                ResetMode.kNoResetSafeParameters,
-                PersistMode.kNoPersistParameters);
+        m_motor = IntakePivotConstants.INTAKE_PIVOT_MOTOR;
     }
 
     public void setSpeed(Dimensionless m_speed) {
-        m_motor.set(m_speed.in(Value));
+        m_motor.setControl(m_dutyCycle.withOutput(m_speed.in(Value)));
     }
 
     public void setAxisSpeed(Dimensionless axisSpeed) {
@@ -31,6 +30,7 @@ public class IntakePivot extends SubsystemBase {
     }
 
     public void stop() {
-        m_motor.stopMotor();
+        m_motor.set(0.0);
     }
+
 }
--- LARR-E/src/main/java/frc/robot/subsystems/intakePivot/IntakePivotConstants.java
+++ LARR-E/src/main/java/frc/robot/subsystems/intakePivot/IntakePivotConstants.java
@@ -5,13 +5,27 @@ import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.units.measure.Dimensionless;
+import frc.robot.constants.CanID;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj.TimedRobot;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.controls.Follower;
+import frc.robot.constants.CanID;
 
 public class IntakePivotConstants {
 
-    public static final SparkBaseConfig MOTOR_CONFIG = new SparkMaxConfig()
-            .inverted(true)
-            .idleMode(IdleMode.kBrake)
-            .smartCurrentLimit(40, 20);
+    public static final TalonFX INTAKE_PIVOT_MOTOR = new TalonFX(CanID.INTAKE_PIVOT_MOTOR);
 
-    public static final Dimensionless AXIS_MAX_SPEED = Value.of(10);
+    private void configureMotor() {
+        TalonFXConfiguration INTAKE_PIVOT_MOTOR_CONFIG = new TalonFXConfiguration();
+        INTAKE_PIVOT_MOTOR_CONFIG.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        INTAKE_PIVOT_MOTOR_CONFIG.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+
+        INTAKE_PIVOT_MOTOR.getConfigurator().apply(INTAKE_PIVOT_MOTOR_CONFIG);
+    }
+
+    public static final Dimensionless AXIS_MAX_SPEED = Value.of(30);
 }
