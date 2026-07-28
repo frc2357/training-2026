896be7126a593b53d8b5d12c346d3edc029100e5
Nathan2351 <nathan.larsen892@raypec.org>
2026-07-28 18:43:18 -0500

    fixed Value misuses

--- LARR-E/src/main/java/frc/robot/controls/DriverControls.java
+++ LARR-E/src/main/java/frc/robot/controls/DriverControls.java
@@ -1,7 +1,7 @@
 package frc.robot.controls;
 
 import static edu.wpi.first.units.Units.Value;
-
+import static edu.wpi.first.units.Units.Percent;
 import edu.wpi.first.units.measure.Dimensionless;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.feeder.FeederSetSpeed;
@@ -23,13 +23,14 @@ public class DriverControls {
     }
 
     public void mapControls() {
-        m_controller.rightTrigger().whileTrue(new IntakePivotAxis(() -> Value.of(m_controller.getRightTriggerAxis())));
-        m_controller.leftTrigger().whileTrue(new IntakePivotAxis(() -> Value.of(-m_controller.getLeftTriggerAxis())));
+        m_controller.rightTrigger()
+                .whileTrue(new IntakePivotAxis(() -> Percent.of(m_controller.getRightTriggerAxis())));
+        m_controller.leftTrigger().whileTrue(new IntakePivotAxis(() -> Percent.of(-m_controller.getLeftTriggerAxis())));
 
-        m_controller.a().onTrue(new ShooterSetSpeed(Value.of(50)));
+        m_controller.a().onTrue(new ShooterSetSpeed(Percent.of(50)));
         m_controller.x().onTrue(new ShooterStop());
-        m_controller.y().whileTrue(new IntakeRunnerSetSpeed(Value.of(50)));
-        m_controller.b().whileTrue(new KickerSetSpeed(Value.of(100)));
+        m_controller.y().whileTrue(new IntakeRunnerSetSpeed(Percent.of(50)));
+        m_controller.b().whileTrue(new KickerSetSpeed(Percent.of(100)));
     }
 
     public Dimensionless getLeftX() {
