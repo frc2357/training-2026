226e6e51dcd127810d7bf5fa65a13f3473c44ffa
Nathan2351 <nathan.larsen892@raypec.org>
2026-08-11 18:25:21 -0500

    testing

--- LARR-E/src/main/java/frc/robot/commands/intakePivot/IntakeDeploy.java
+++ LARR-E/src/main/java/frc/robot/commands/intakePivot/IntakeDeploy.java
@@ -7,11 +7,16 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Robot;
 import frc.robot.subsystems.intakePivot.IntakePivotConstants;
 
 public class IntakeDeploy extends Command {
 
     public IntakeDeploy() {
+        addRequirements(Robot.intakePivot);
+    }
+
+    public void execute() {
         new IntakePivotSetSpeed(Percent.of(40)).andThen(
                 new WaitCommand(IntakePivotConstants.DEPLOY_TIME.in(Value))).andThen(
                         new IntakePivotSetSpeed(Percent.of(0)));
