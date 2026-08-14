90567b0227555036be9cce32fb8fbfe43069a158
Nathan2351 <nathan.larsen892@raypec.org>
2026-08-13 19:49:19 -0500

    it goes right way now

--- LARR-E/src/main/java/frc/robot/subsystems/intakeRunner/IntakeRunner.java
+++ LARR-E/src/main/java/frc/robot/subsystems/intakeRunner/IntakeRunner.java
@@ -28,9 +28,9 @@ public class IntakeRunner extends SubsystemBase {
         TalonFXConfiguration leftConfig = new TalonFXConfiguration();
         TalonFXConfiguration rightConfig = new TalonFXConfiguration();
         leftConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
-        leftConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+        leftConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
         rightConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
-        rightConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        rightConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
         m_leftMotor.getConfigurator().apply(leftConfig);
         m_rightMotor.getConfigurator().apply(rightConfig);
     }
