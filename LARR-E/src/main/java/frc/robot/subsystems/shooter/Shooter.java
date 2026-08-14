6bc9a8b1f76670c5e25247f25a6ffa305461fb43
Nathan2357 <nathan.larsen892@raypec.org>
2026-08-13 19:56:50 -0500

    Updated the shooter system and tested it. Made improvements to other … (#47)
    * Updated the shooter system and tested it. Made improvements to other kraken subsystems.
    
    * it goes right way now
    
    * Actually goes right way now.

--- LARR-E/src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ LARR-E/src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -2,6 +2,9 @@ package frc.robot.subsystems.shooter;
 
 import static edu.wpi.first.units.Units.Value;
 import com.revrobotics.spark.SparkMax;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -9,24 +12,25 @@ import edu.wpi.first.units.measure.Dimensionless;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.CanID;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.controls.StrictFollower;
+
 public class Shooter extends SubsystemBase {
 
-    private SparkMax m_leftMotor;
-    private SparkMax m_rightMotor;
+    public static final TalonFX m_leftMotor = new TalonFX(CanID.LEFT_SHOOTER_MOTOR);
+    public static final TalonFX m_rightMotor = new TalonFX(CanID.RIGHT_SHOOTER_MOTOR);
 
     public Shooter() {
-        m_leftMotor = new SparkMax(CanID.LEFT_SHOOTER_MOTOR, MotorType.kBrushless);
-        m_rightMotor = new SparkMax(CanID.RIGHT_SHOOTER_MOTOR, MotorType.kBrushless);
-
-        m_leftMotor.configure(
-                ShooterConstants.LEFT_MOTOR_CONFIG,
-                ResetMode.kNoResetSafeParameters,
-                PersistMode.kNoPersistParameters);
-
-        m_rightMotor.configure(
-                ShooterConstants.RIGHT_MOTOR_CONFIG,
-                ResetMode.kNoResetSafeParameters,
-                PersistMode.kNoPersistParameters);
+        m_rightMotor.setControl(new StrictFollower(m_leftMotor.getDeviceID()));
+        TalonFXConfiguration leftConfig = new TalonFXConfiguration();
+        TalonFXConfiguration rightConfig = new TalonFXConfiguration();
+        leftConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+        leftConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+        rightConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+        rightConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        m_leftMotor.getConfigurator().apply(leftConfig);
+        m_rightMotor.getConfigurator().apply(rightConfig);
     }
 
     public void setSpeed(Dimensionless m_speed) {
